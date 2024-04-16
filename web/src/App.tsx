7da58676bc6d1b81a62beb16324bcb91deb981a4
Luc <luc@lucemans.nl>
2024-04-16 04:51:42 +0000

    Bump

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,12 @@
 export const App = () => {
 
     return (
-        <div
-            className="">
-            Will is going to win, but will you?
+        <div className="w-full">
+            <div className="w-full max-w-xl p-4 mx-auto">
+                <div className="w-full border rounded-xl p-4 border-light-border bg-light-background-primary">
+                    Will is going to win, but will you?
+                </div>
+            </div>
         </div>
     );
 }
